32bbb8ac3fc8a6203e4f84f5947c33a3f2cf6f13
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 00:37:40 +0500

    inti

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var score = 99.5
+
+// go run main.go greetings.go
 func main() {
-	fmt.Println("Hello world")
+	sayHello("Mario")
+
+	for _, p := range points {
+		fmt.Println(p)
+	}
+
+	showScore()
 }
