7e0a2430924bcb13f49cae68b74404d8b245a0cd
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 00:32:04 +0500

    init

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(name string) (string, string) {
+	s := strings.ToUpper(name)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, initial := range names {
+		initials = append(initials, initial[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], ""
+
+}
 
 func main() {
-	fmt.Println("Hello world")
+	firstName, secondName := getInitials("Usain bolt")
+	fmt.Printf("Users first name is %v and second name is %v \n", firstName, secondName)
+
+	firstName, secondName = getInitials("Mario")
+	fmt.Printf("Users first name is %v and second name is %v \n", firstName, secondName)
 }
