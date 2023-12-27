546b8ae2baccb511d884ef502b4638e6f792cec1
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 00:54:25 +0500

    init

--- main.go
+++ main.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+// return "piper"
+func updateName(name string) string {
+	name = "piper"
+	return name
+}
+
+// adds ice cream to the menu
+func updateMenu(menu map[string]float64) {
+	menu["ice cream"] = 12.99
+}
+
 func main() {
-	fmt.Println("Hello world")
+	// group A types => strings, ints, bools, floats, arrays, structs (non pointer values)
+	name := "carl"
+
+	updateName(name)
+
+	fmt.Println(name) // carl
+
+	name = updateName(name)
+
+	fmt.Println(name) // piper
+
+	// group B types => slices, maps, functions (pointer wrapper values)
+	menu := map[string]float64{
+		"pie":    5.49,
+		"coffee": 8.99,
+	}
+
+	updateMenu(menu)
+
+	fmt.Println(menu) // map[coffee:8.99 ice cream:12.99 pie:5.49]
 }
