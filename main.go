f0879d58a4d8514f652c89fb287979f57ce1712c
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 01:06:11 +0500

    init

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func updateName(name *string) {
+	*name = "piper"
+}
+
 func main() {
-	fmt.Println("Hello world")
+	name := "carl"
+
+	// updateName(name)
+
+	// fmt.Println("memory address of name is:", &name)
+
+	m := &name
+
+	fmt.Println("value at memory address", *m)
+
+	updateName(m)
+
+	fmt.Println("value at memory address", *m)
 }
