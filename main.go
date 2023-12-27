6110ef989cac3c28ed6db6d1f578ddba9be38eb6
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 00:44:33 +0500

    init

--- main.go
+++ main.go
@@ -3,5 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	menu := map[string]float64{
+		"soup":   4.99,
+		"pie":    7.99,
+		"salad":  6.99,
+		"toffee": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["soup"])
+
+	// looping maps
+
+	for key, value := range menu {
+		fmt.Println(key, "-", value)
+	}
+
+	// ints as key type
+
+	phonebook := map[int]string{
+		1208021: "mario",
+		3209823: "luigi",
+		2398102: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[2398102])
+
+	phonebook[3209823] = "bowser"
+
+	fmt.Println(phonebook[3209823])
+
 }
