0bef459cfc8d365469cd618d03cffeeee1caa000
TheOreoTM <qwertzybad@gmail.com>
2023-12-27 16:54:27 +0500

    init

--- main.go
+++ main.go
@@ -3,5 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+
+	// strings
+	// var name string = "Bar"
+	// var name2 = "Bar2"
+	// name3 := "Foo"
+	// var name4 string
+
+	// fmt.Println(name, name2, name3, name4)
+	
+	// name = "John"
+	// name4 = "Adam"
+
+	// fmt.Println(name, name2, name3, name4)
+
+
+	// ints
+
+	fmt.Println("\nInts")
+	var ageOne int = 30
+	var ageTwo = 20
+	ageThree := 10
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits and memory
+
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint8 = 41
+	var scoreTwo float64 = 1984398733294283974283742834728347934792837837492837498237498327493287498237498378273498739847239874923743279472347237473248723987398734972398748397428784327623742364276423738764863287638724928749812737249827498274987298274.21498734987329483724
+
+	fmt.Println("\nBits and memory")
+	fmt.Println(numOne, numTwo, numThree, scoreTwo)
+
+
 }
