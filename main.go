4e74fe748d47088d3c58d527bc5e6d2e9762a598
TheOreoTM <qwertzybad@gmail.com>
2023-12-28 00:09:25 +0500

    init

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Println() prints and adds a \n at the end fmt.Print() doesnt
+
+	const text string = "hello there"
+	const age int8 = 20
+	const name string = "Foo"
+
+	fmt.Println("my age is", age, "and my name is", name)
+
+	// Printf - (formatted strings) %_ = format specifier
+
+	fmt.Printf("my age is %q and my name is %q \n", age, name)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is:", str)
+
 }
